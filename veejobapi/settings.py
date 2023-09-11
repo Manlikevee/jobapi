7c4ed448964a2d5dcf0a2f25567a2431253e26ab
manlikevee <odahviktor@gmail.com>
2023-09-11 12:28:31 +0100

    vee first commits

--- veejobapi/settings.py
+++ veejobapi/settings.py
@@ -74,11 +74,13 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'veejobapi.middleware.UpdateLastSeenMiddleware',
 ]
 
 ROOT_URLCONF = 'veejobapi.urls'
