818149c93a216034fa01a42124c8cc7aa2beb3b7
manlikevee <odahviktor@gmail.com>
2024-08-06 11:52:06 +0100

    vee

--- dashboard/serializer.py
+++ dashboard/serializer.py
@@ -17,7 +17,7 @@ class Jobserializer(serializers.ModelSerializer):
     organization = companyserializer()
     class Meta:
         model = Jobs
-        exclude = ['applied', 'payment_data', 'is_paidfor']  # Or specify the fields you want to expose
+        fields = ['jobtitle', 'jobservice', 'joblocation', 'jobcategory', 'jobsalaryrange', 'workinglevel', 'jobdescription', 'responsibilities', 'requirements', 'organization']  # Or specify the fields you want to expose
 
 
 
